180112a14a8a2892e2214276baef347e325e46f5
deepanjali1206 <deepanjalisingh0612@gmail.com>
2025-05-24 17:02:46 +0530

    User Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
-require('dotenv').config();
- 
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+require("dotenv").config();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
- 
-app.get('/api/test',(req,res)=>{
-    res.send('API is working');
-})
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
-.then(() =>console.log('mongodb connected'))
-.catch((err)=>console.error('mongodb connection error:',err));
+app.use("/api", authRoutes);
+app.get("/api/test", (req, res) => {
+  res.send("API is working");
+});
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>console.log('Server running on port ${PORT}'));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.error("mongodb connection error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port ${PORT}"));
